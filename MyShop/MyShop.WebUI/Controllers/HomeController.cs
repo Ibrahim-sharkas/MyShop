95c5609159962cbee9eee073b5de48b1d7b391fb
Ibrahim-sharkas <isharckas@yahoo.com>
2021-11-26 02:21:35 +0200

    AddingFilterToProducts

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,47 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        IReposetory<Product> context;
+        IReposetory<ProductCatagory> catContext;
+        public HomeController(IReposetory<Product> contextProduct,IReposetory<ProductCatagory> contextCat)
         {
-            return View();
+            this.context = contextProduct;
+            this.catContext = contextCat;
+        }
+
+        public HomeController()
+        {
+        }
+
+        public ActionResult Index(string catagory=null)
+        {
+            List<Product> products ;
+            List<ProductCatagory> catagories = catContext.Collection().ToList();
+            if(catagory== null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Catagorey == catagory).ToList();
+            }
+            ListViewModel model = new ListViewModel();
+            model.Products = products;
+            model.ProductCatagories = catagories;
+
+            return View(model);
+        }
+        public ActionResult Details(string id)
+        {
+            Product p = context.Find(id);
+            if (p == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(p);
+            }
         }
 
         public ActionResult About()
